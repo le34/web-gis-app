fbcc084ef50617b51ce3d31e98eda4cde499ffd9
Rune Tvilum <rune@addin.dk>
2017-11-21 13:08:01 +0100

    drone

--- server/api/tiles.js
+++ server/api/tiles.js
@@ -24,7 +24,7 @@ router.get('/:id', function (req, res) {
         if (err || !info) {
           return res.status(404).send('ERROR: Metadata missing in the MBTiles.')
         }
-        info.tiles = [ urlPrefix + '/api/tiles/' + req.params.id + '/{z}/{x}/{y}.pbf' ]
+        info.tiles = [urlPrefix + '/api/tiles/' + req.params.id + '/{z}/{x}/{y}.pbf']
         res.json(info)
       })
     })
@@ -34,6 +34,7 @@ router.get('/:id/:z(\\d+)/:x(\\d+)/:y(\\d+).:format([\\w.]+)', function (req, re
   const z = req.params.z | 0
   const x = req.params.x | 0
   const y = req.params.y | 0
+  const format = req.params.format
   const mbtilesFile = path.join(filePath, req.params.id + '.mbtiles')
   const source = new MBTiles(mbtilesFile, function (err) {
     if (err) {
@@ -47,19 +48,23 @@ router.get('/:id/:z(\\d+)/:x(\\d+)/:y(\\d+).:format([\\w.]+)', function (req, re
           return res.status(500).send(err.message)
         }
       }
-      res.set('Content-Encoding', 'gzip')
-      res.set('Content-Type', 'application/x-protobuf')
-
-      const isGzipped = data.slice(0, 2).indexOf(Buffer.from([0x1f, 0x8b])) === 0
-      if (isGzipped) {
-        return res.send(data)
-      }
-      data = zlib.gzip(data, (err, result) => {
-        if (err) {
-          return res.status(500).json(err)
+      if (format === 'png') {
+        res.set('Content-Type', 'image/png')
+        res.send(data)
+      } else {
+        res.set('Content-Encoding', 'gzip')
+        res.set('Content-Type', 'application/x-protobuf')
+        const isGzipped = data.slice(0, 2).indexOf(Buffer.from([0x1f, 0x8b])) === 0
+        if (isGzipped) {
+          return res.send(data)
         }
-        res.send(result)
-      })
+        data = zlib.gzip(data, (err, result) => {
+          if (err) {
+            return res.status(500).json(err)
+          }
+          res.send(result)
+        })
+      }
     })
   })
 })
